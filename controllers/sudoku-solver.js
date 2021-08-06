e9c6b33244ac22e121ccd3c45138a8eb4b595720
David <DRhoton@vinyacastelldefels.com>
2021-08-06 16:52:46 +0200

    First push

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,22 +1,168 @@
+// Based on walkthrough with Landon Schlangen:
+// https://www.youtube.com/watch?v=6XDcvG2ZCRc
+
 class SudokuSolver {
 
   validate(puzzleString) {
-  }
-
-  checkRowPlacement(puzzleString, row, column, value) {
 
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
-
+  checkRowPlacement(puzzleString, row, col, value) {
+    let grid = this.transform(puzzleString);
+    row = row.charCodeAt(0) - 64;  // A = Unicode 65; thus -> 1
+    if (grid[row-1][col-1] != 0) return false;  //Check if number already there
+    for (let i = 0; i < 9; i++) {
+      if (grid[row-1][i] == value) return false  
+    };
+    return true;
   }
 
-  checkRegionPlacement(puzzleString, row, column, value) {
-
+  checkColPlacement(puzzleString, row, col, value) {
+    let grid = this.transform(puzzleString);
+    row = row.charCodeAt(0) - 64;  // A = Unicode 65; thus -> 1
+    if (grid[row-1][col-1] != 0) return false;  //Check if number already there
+    for (let i = 0; i < 9; i++) {
+      if (grid[i][col-1] == value) return false  
+    };
+    return true;
   }
+
+  checkRegionPlacement(puzzleString, row, col, value) {
+    let grid = this.transform(puzzleString);
+    row = row.charCodeAt(0) - 64;  // A = Unicode 65; thus -> 1
+    if (grid[row-1][col-1] != 0) return false;  //Check if number already there
+    let startRow = row - (row % 3);
+    let startCol = col - (col % 3);
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (grid[i + startRow][j + startCol] == value) return false  
+      };
+    };
+    return true;
+  }
+
+  // Algorithm from https://www.geeksforgeeks.org/sudoku-backtracking-7/
+
+  /* Takes a partially filled-in grid and attempts to
+  assign values to all unassigned locations in such a
+  way to meet the requirements for Sudoku solution
+  (non-duplication across rows, columns, and boxes) ;
+  row and col indicate the starting point, usually 0, 0. */
+  solveSuduko(grid, row, col) {
+    
+    // N is the size of the 2D matrix N*N
+    let N = 9;
+    
+    /* If we have reached the 8th row and 9th column
+    (0 indexed matrix), we are returning true to
+    avoid further backtracking */
+    if (row == N - 1 && col == N) return grid;
+
+    // Check if column value becomes 9 ,
+    // we move to next row and column start from 0
+    if (col == N) {
+      row++;
+      col = 0;
+    }
+
+    // Check if the current position of the grid
+    // already contains value >0, we iterate
+    // for next column
+    if (grid[row][col] != 0)
+      return this.solveSuduko(grid, row, col + 1);
+
+    for(let num = 1; num < 10; num++) {
+      
+      // Check if it is safe to place the num (1-9)
+      // in the given row , col 
+      // -> we move to next column
+      if (this.isSafe(grid, row, col, num)) {
+        
+        /* assigning the num in the current (row,col)
+        position of the grid and assuming our assigned
+        num in the position is correct */
+        grid[row][col] = num;
+
+        // Checking for next possibility with next column
+        if (this.solveSuduko(grid, row, col + 1))
+          return grid;
+      }
+      
+      /* removing the assigned num , since our assumption
+      was wrong , and we go for next assumption with
+      diff num value */
+      grid[row][col] = 0;
+    }
+    return false;
+  }
+
+  /* Check whether it will be legal to assign num to
+  the given row, col */
+  isSafe(grid, row, col, num) {
+    
+    /* Check if we find the same num in the same row ,
+    we return false */
+    for(let x = 0; x <= 8; x++)
+      if (grid[row][x] == num)
+        return false;
+
+    /* Check if we find the same num in the similar column ,
+    we return false */
+    for(let x = 0; x <= 8; x++)
+      if (grid[x][col] == num)
+        return false;
+
+    /* Check if we find the same num in the particular 3*3
+    matrix, we return false */
+    let startRow = row - row % 3,
+      startCol = col - col % 3;
+      
+    for(let i = 0; i < 3; i++)
+      for(let j = 0; j < 3; j++)
+        if (grid[i + startRow][j + startCol] == num)
+          return false;
+
+    return true;
+  }
+
+  transform(puzzleString) {
+    /* transform '..57.23...61.' into 
+    [[0,0,5,7,0,2,3,0,0],
+     [0,6,1,0,         ]] and so on */
+    let grid = [
+      [0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0]
+    ];
+    let row = -1;
+    let col = 0;
+    for (let i=0; i< puzzleString.length; i++) {
+      if ( i % 9 == 0 ) row++;
+      if ( col % 9 == 0 ) col =0;
+      grid[row][col] = puzzleString[i] === "." ? 0 : +puzzleString[i];
+      col++;
+    }
+    return grid;
+  }
+
+  transformBack(grid) {
+    return grid.flat().join("");
+  }
+
 
   solve(puzzleString) {
-    
+    let grid = this.transform(puzzleString);
+    let solved = this.solveSuduko(grid, 0, 0);
+    if (!solved) return false;
+    let solvedString = this.transformBack(solved);
+    console.log("solvedString: " + solvedString)
+    return solvedString;
   }
 }
 
--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,39 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
+      const { puzzle, coordinate, value} = req.body;  // see comment in next section
+      if (!puzzle || !coordinate || !value) return res.json({ error: "Required field(s) missing" });
+      let row = coordinate.split("")[0].toUpperCase();
+      let col = coordinate.split("")[1];
+      if ( coordinate.length !== 2 || !/[A-I]/.test(row) || !/[1-9]/.test(col) ) {
+        return res.json({ error: "Invalid coordinate" });
+      }
+      if ( !/[1-9]/.test(value) || value.length != 1 ) return res.json({ error: "Invalid value" });
+      if (puzzle.length != 81) return res.json({ error: "Expected puzzle to be 81 characters long" });
+      if (/[^0-9.]/g.test(puzzle)) return res.json({ error: "Invalid characters in puzzle" });
 
+      let validRow = solver.checkRowPlacement(puzzle, row, col, value);
+      let validCol = solver.checkColPlacement(puzzle, row, col, value);
+      let validReg = solver.checkRegionPlacement(puzzle, row, col, value);
+      let conflicts = [];
+      if (validRow && validCol && validReg) return res.json({ valid: true });
+      if (!validRow) conflicts.push("row");
+      if (!validCol) conflicts.push("column");
+      if (!validReg) conflicts.push("region");
+      return res.json({ valid: false, conflict: conflicts});
+      
     });
     
   app.route('/api/solve')
     .post((req, res) => {
+      let puzzle = req.body.puzzle;  // Or, let { puzzle } = req.body
+      if (!puzzle) return res.json({ error: "Required field missing" });
+      if (puzzle.length != 81) return res.json({ error: "Expected puzzle to be 81 characters long" });
+      if (/[^0-9.]/g.test(puzzle)) return res.json({ error: "Invalid characters in puzzle" });
+
+      let solvedString = solver.solve(puzzle);
+      if (!solvedString) return res.json({ error: "Puzzle cannot be solved" });
+      res.json({ solution: solvedString });
 
     });
 };
